586ee7de3bd93cf3717269b9f79bb939d1400f68
Marko <ms7538@gmail.com>
2018-03-12 22:02:01 -0600

    progress bar display complete! progress bar touch/seek in progress!

--- app/src/main/java/com/poloapps/rehls/MainActivity.java
+++ app/src/main/java/com/poloapps/rehls/MainActivity.java
@@ -1,6 +1,7 @@
 package com.poloapps.rehls;
 
 import android.app.ProgressDialog;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Handler;
@@ -8,28 +9,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+
 //git check
 public class MainActivity extends AppCompatActivity {
-    Handler mHandler;
-    private Runnable mRunnable = new Runnable() {
-        @Override
-        public void run() {
-            setCurrentPosition();
-            mHandler.postDelayed(mRunnable, 1000);
-        }
-    };
-    int count = 0;
-    ProgressDialog mDialog;
-    VideoView      videoView;
-    ImageButton    btnPlayPause;
+    Handler           mHandler;
+    int               count = 0;
+    ProgressDialog    mDialog;
+    VideoView         videoView;
+    ImageButton       btnPlayPause;
 
     int    stopPosition = 0;
     int    currPosition = 0;
-    int        duration = 0;
+    int    progPosition = 0;
+    int        duration = 1;
     String timeDuration = "0:00";
     String currDuration = "0:00";
 
@@ -39,15 +36,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        useHandler();
-        mHandler.removeCallbacks(mRunnable);
         final TextView durationTime = findViewById(R.id.duration_time);
+
         videoView    = findViewById(R.id.videoView);
         btnPlayPause = findViewById(R.id.play_pause_btn);
         mDialog      = new ProgressDialog(MainActivity.this );
         mDialog.setMessage("Please Wait ...");
         mDialog.setCanceledOnTouchOutside(false);
-
         btnPlayPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -87,12 +82,13 @@ public class MainActivity extends AppCompatActivity {
                         mp.setLooping(true);
                         if(stopPosition > 0) videoView.seekTo(stopPosition);
                         duration = videoView.getDuration() / 1000;
-                        if(duration < 60){
-                            timeDuration = "0:" + duration;
-                        }else{
+                        if(duration < 10) timeDuration = "0:0" + duration;
+                        else if (duration < 60) timeDuration = "0:" + duration;
+                        else{
                             int mins = duration / 60 ;
                             int sec  = duration % 60 ;
-                            timeDuration = mins + ":" + sec;
+                            if (sec < 10) timeDuration = mins + ":0" + sec;
+                            else timeDuration = mins + ":" + sec;
                         }
                         durationTime.setText(timeDuration);
                         useHandler();
@@ -103,32 +99,46 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
-
-
     }
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            setCurrentPosition();
+            mHandler.postDelayed(mRunnable, 1000);
+        }
+    };
     public void useHandler() {
         mHandler = new Handler();
         mHandler.postDelayed(mRunnable, 1000);
     }
     private void setCurrentPosition(){
-        Toast.makeText(getBaseContext(), Integer.toString(count),
-                Toast.LENGTH_SHORT).show();
         count++;
+        final ProgressBar progressBar = findViewById(R.id.Progressbar);
+        progressBar.setBackgroundColor(Color.CYAN);
         final TextView currentTime = findViewById(R.id.current_time);
-        currPosition = videoView.getCurrentPosition() / 1000;
-        if (currPosition < 60) {
-            currDuration = "0:" + currPosition;
-        } else {
+
+        currPosition    = videoView.getCurrentPosition() / 1000;
+        progPosition    = currPosition * 100 / duration;
+        progressBar.setProgress(progPosition);
+
+        if(currPosition < 10) currDuration = "0:0" + currPosition;
+        else if (currPosition < 60) currDuration = "0:" + currPosition;
+        else {
             int mins = currPosition / 60;
             int sec = currPosition % 60;
-            currDuration = mins + ":" + sec;
+            if (sec < 10) currDuration = mins + ":0" + sec;
+            else currDuration = mins + ":" + sec;
+
         }
         currentTime.setText(currDuration);
     }
     @Override
     public void onPause() {
         super.onPause();
-        mHandler.removeCallbacks(mRunnable);
+        try {
+            mHandler.removeCallbacks(mRunnable);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
-
 }
